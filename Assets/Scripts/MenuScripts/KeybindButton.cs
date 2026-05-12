7640ea7af8b5e53b9fa73b0d21ccc0ac66cbaeb1
boygenius2 <ivang85601@gmail.com>
2026-05-11 23:25:01 -0700

    UPDATE!! Keyboard rebinding with ALL NEW InputSystem, and UPDATED UI!!!!!
    Just need to make the player load in their saved keybinds when they spawn, and I also gotta do controller rebinding soon after. And maybe another small update to the controller bindings section. Oh, and when loaded, don't leave the buttons with the default bindings, and to get the reset to keyboard defaults working.

--- Assets/Scripts/MenuScripts/KeybindButton.cs
+++ Assets/Scripts/MenuScripts/KeybindButton.cs
@@ -4,20 +4,42 @@ using System;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class KeybindButton : MonoBehaviour
 {
+    // Using new input system stuff
+    public InputActionAsset playerInputActions;
+    private InputActionRebindingExtensions.RebindingOperation _rebindingOperation;
+    private InputAction _moveAction;
+    private InputAction _singleButtonAction;
+    private Button _buttonSelected;
+
+    // Old stuff
     public TextMeshProUGUI buttonText;
     [SerializeField] private GameObject _keybindMenu;
-    private Button _buttonSelected;
     private string _keyMapName;
-    private List<Button> _keyboardButtons = new List<Button>();
-    private List<Button> _controllerButtons = new List<Button>();
     public static bool isMakingInput = false;
 
+    // Button lists for rebinding
+    private List<Button> _keyboardButtons = new List<Button>();
+    private List<Button> _controllerButtons = new List<Button>();
 
+    void Awake()
+    {
+        _moveAction = playerInputActions.FindAction("Move");
+
+        string rebinds = PlayerPrefs.GetString("rebinds");
+        // Debug.Log(rebinds);
+        if (!string.IsNullOrEmpty(rebinds))
+        {
+            playerInputActions.LoadBindingOverridesFromJson(rebinds);
+        }
+    }
+    
     void Start()
     {
+        // Get a list of all buttons availble for rebinding
         Button[] buttons = _keybindMenu.GetComponentsInChildren<Button>();
         foreach (Button button in buttons)
         {
@@ -26,18 +48,11 @@ public class KeybindButton : MonoBehaviour
             else if (button.gameObject.name.Contains("Cont") && !button.gameObject.name.Contains("Default"))
                 _controllerButtons.Add(button);
         }
-        LoadUserSetKeybinds();
+        // LoadUserSetKeybinds();
     }
 
-    void Update()
-    {
-        if (buttonText.text == "Set New Input")
-        {
-            isMakingInput = true;
-            StartCoroutine(KeyPressInput()); 
-        }
-    }
-
+    
+    // Searches for a given button name within the list of buttons, 
     public void EditButtonByName(string searchButtonName)
     {
         Button[] buttons = _keybindMenu.GetComponentsInChildren<Button>();
@@ -46,76 +61,119 @@ public class KeybindButton : MonoBehaviour
             if (button.gameObject.name == searchButtonName)
             {
                 _buttonSelected = button;
+                playerInputActions.FindActionMap("Player").Disable();
                 break;
             }
         }
         buttonText = _buttonSelected.GetComponentInChildren<TextMeshProUGUI>();
-    }
-
-    public void SetNewKeybind(string keybindName)
-    {
         buttonText.text = "Set New Input";
-        _keyMapName = keybindName;
     }
 
-    public void ResetKeyboardKeybindsToDefault()
-    {
-        Dictionary<string, KeyCode> defaultMapping = new Dictionary<string, KeyCode>(CustomInputManager.keyMapping);
-        KeyCode[] defaultKeys = CustomInputManager.GetDefaultKeys();
-        int i = 0;
-
-        foreach(string keyName in CustomInputManager.keyMapping.Keys)
+    public void SetNewKeybind(string actionName)
+    {        
+        playerInputActions.FindActionMap("Player").Disable();
+        _buttonSelected.interactable = false;
+        
+        if ( actionName.Contains("Left") || actionName.Contains("Right") )
         {
-            buttonText = _keyboardButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            defaultMapping[keyName] = defaultKeys[i];
-            buttonText.text = defaultKeys[i].ToString();
-            PlayerPrefs.SetInt(keyName, (int) defaultKeys[i]);
-            PlayerPrefs.Save();
-            i++;
+            InputActionSetupExtensions.BindingSyntax movementKeys = _moveAction.ChangeCompositeBinding("WASD");  // Gets the composite named "WASD", which contains all the movement key bindings
+            int keyBindIndex = -1;
+            switch (actionName)
+            {
+                case "Left1":
+                    keyBindIndex = movementKeys.NextPartBinding("Left").bindingIndex;  // First left binding
+                    break;
+                case "Left2":
+                    keyBindIndex = movementKeys.NextPartBinding("Left").bindingIndex + 1;  // Second left binding
+                    break;
+                case "Right1":
+                    keyBindIndex = movementKeys.NextPartBinding("Right").bindingIndex;  // First right binding
+                    break;
+                case "Right2":
+                    keyBindIndex = movementKeys.NextPartBinding("Right").bindingIndex + 1;  // Second right binding
+                    break;
+            }
+            _rebindingOperation = _moveAction.PerformInteractiveRebinding(keyBindIndex).OnComplete(operation => KeyRebindingComplete(actionName, keyBindIndex));
         }
-        CustomInputManager.keyMapping = defaultMapping;
+        else
+        {
+            _singleButtonAction = playerInputActions.FindAction(actionName);
+            _rebindingOperation = _singleButtonAction.PerformInteractiveRebinding(1).OnComplete(operation => KeyRebindingComplete(actionName));
+        }
+        
+        _rebindingOperation.Start();
     }
+
+    private void KeyRebindingComplete(string actionName, int compKeybindIndex = -1)
+    {
+        _rebindingOperation.Dispose();
+        _buttonSelected.interactable = true;
+
+        string newBinding;
+        if ( actionName.Contains("Left") || actionName.Contains("Right") )
+            newBinding = _moveAction.bindings[compKeybindIndex].effectivePath;
+        else
+            newBinding = _singleButtonAction.bindings[1].effectivePath;
+        
+        // Formatting of the binding path to just show the key pressed
+        short index = (short) newBinding.Length;
+        while (newBinding[index-1] != '/')
+            index--;
+        newBinding = newBinding[index..].ToUpper();
+        buttonText.text = newBinding;
+        
+        playerInputActions.FindActionMap("Player").Enable();
+
+        // Save keybind afterwards
+        string rebinds = playerInputActions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString("rebinds", rebinds);
+    }
+
+    public void SetNewControllerBind()
+    {
+        playerInputActions.FindActionMap("Player").Disable();
+        _buttonSelected.interactable = false;
+    }
+
+    private void ControllerRebindingComplete()
+    {
+        _rebindingOperation.Dispose();
+        _buttonSelected.interactable = true;
+    }
+
+    // // Reload everything in this section later
+    // public void ResetKeyboardKeybindsToDefault()
+    // {
+    //     Dictionary<string, KeyCode> defaultMapping = new Dictionary<string, KeyCode>(CustomInputManager.keyMapping);
+    //     KeyCode[] defaultKeys = CustomInputManager.GetDefaultKeys();
+    //     int i = 0;
+
+    //     foreach(string keyName in CustomInputManager.keyMapping.Keys)
+    //     {
+    //         buttonText = _keyboardButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+    //         defaultMapping[keyName] = defaultKeys[i];
+    //         buttonText.text = defaultKeys[i].ToString();
+    //         PlayerPrefs.SetInt(keyName, (int) defaultKeys[i]);
+    //         PlayerPrefs.Save();
+    //         i++;
+    //     }
+    //     CustomInputManager.keyMapping = defaultMapping;
+    // }
 
     // When the game loads, load up all of the user set keybinds
-    public void LoadUserSetKeybinds()
-    {
-        Button[] buttons = _keybindMenu.GetComponentsInChildren<Button>();
-        Dictionary<string, KeyCode> userMapping = new Dictionary<string, KeyCode>(CustomInputManager.keyMapping);
-        int i = 0;
+    // public void LoadUserSetKeybinds()
+    // {
+    //     Button[] buttons = _keybindMenu.GetComponentsInChildren<Button>();
+    //     Dictionary<string, KeyCode> userMapping = new Dictionary<string, KeyCode>(CustomInputManager.keyMapping);
+    //     int i = 0;
 
-        foreach(string keyName in CustomInputManager.keyMapping.Keys)
-        {
-            buttonText = buttons[i].GetComponentInChildren<TextMeshProUGUI>();
-            userMapping[keyName] = (KeyCode) PlayerPrefs.GetInt(keyName);
-            buttonText.text = ((KeyCode) PlayerPrefs.GetInt(keyName)).ToString();
-            i++;
-        }
-        CustomInputManager.keyMapping = userMapping;
-    }
-
-    // Coroutine method:
-    // This is to avoid the player from exiting out of the pause menu after setting a keybind for pausing the game
-    IEnumerator NotMakingInput()
-    {
-        yield return null;
-        isMakingInput = false;
-    }
-    
-    // Coroutine method:
-    // This one is to avoid setting the keybind right after the player wants to set a new keybind by pressing any confirm key
-    IEnumerator KeyPressInput()
-    {
-        yield return null;
-        foreach(KeyCode keycode in Enum.GetValues(typeof(KeyCode)))
-        {
-            if (Input.GetKeyDown(keycode))
-            {
-                buttonText.text = keycode.ToString();
-                CustomInputManager.SetKeyMap(_keyMapName, keycode);
-                StartCoroutine(NotMakingInput());
-                PlayerPrefs.SetInt(_keyMapName, (int) keycode);
-                PlayerPrefs.Save();
-            }
-        }
-    }
+    //     foreach(string keyName in CustomInputManager.keyMapping.Keys)
+    //     {
+    //         buttonText = buttons[i].GetComponentInChildren<TextMeshProUGUI>();
+    //         userMapping[keyName] = (KeyCode) PlayerPrefs.GetInt(keyName);
+    //         buttonText.text = ((KeyCode) PlayerPrefs.GetInt(keyName)).ToString();
+    //         i++;
+    //     }
+    //     CustomInputManager.keyMapping = userMapping;
+    // }
 }
